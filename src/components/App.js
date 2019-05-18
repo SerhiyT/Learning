91b5adc8b45d04f65c7d9cfd7fd23eae01209fca
SerhiyT <s.serhiy.t@gmail.com>
2019-05-18 09:57:24 +0300

    CompStyleCSS

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import articles from '../fixtures'
 import ArticleList from './ArticleList'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
-    <div>
+    <div className="container">
+      <div className="jumbotron">
+        <h1 className="display-4">App NAME</h1>
+      </div>
       <ArticleList articles = {articles} />
     </div>
   )
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 class Article extends Component {
 
@@ -7,17 +7,25 @@ class Article extends Component {
   }
 
   render() {
-    const {article} = this.props
-    const body = this.state.isOpen && <section>{article.text}</section>
-    
+    const { article } = this.props
+    const body = this.state.isOpen && <section className="card-text">{article.text}</section>
+    const style = { width: '50%' }
     return (
-      <div>
-        <h2>
-        {article.title}
-        <button onClick={this.handleClick}>{this.state.isOpen ? 'Close' : 'Open'}</button>
-        </h2>
-         {body}
-         <h3> Creation date: {(new Date(article.date)).toDateString()}</h3>
+      <div className="card" style={style}>
+        <div className="card-header">
+          <h2>
+            {article.title}
+            <button onClick={this.handleClick} className="btn btn-primary float-right">
+              {this.state.isOpen ? 'Close' : 'Open'}
+            </button>
+          </h2>
+        </div>
+        <div className="card-body">
+          <h6 className="card-subtitle text-muted">
+            Creation date: {(new Date(article.date)).toDateString()}
+          </h6>
+          {body}
+        </div>
       </div>
     )
   }
